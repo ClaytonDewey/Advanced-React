e73d984fa89605fbf2434f6326b21805e23f6e20
ClaytonDewey <clay@dryadmedia.com>
2026-03-23 12:04:14 -0500

    Refactor PaginationFields

--- sick-fits/frontend/components/Products.js
+++ sick-fits/frontend/components/Products.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Product from './Product';
 import { perPage } from '../config';
+import DisplayError from './ErrorMessage';
 
 export const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
@@ -36,7 +37,7 @@ export default function Products({ page }) {
   });
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  if (error) return <DisplayError error={error} />;
   return (
     <div>
       <ProductsListStyles>
