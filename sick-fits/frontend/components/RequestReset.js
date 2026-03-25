a700562b3f8456656507b79c448c89feeffd0a15
ClaytonDewey <clay@dryadmedia.com>
2026-03-25 14:53:23 -0500

    Update Reset Request Form

--- sick-fits/frontend/components/RequestReset.js
+++ sick-fits/frontend/components/RequestReset.js
@@ -1,8 +1,7 @@
-import Form from './styles/Form';
-import useForm from '../lib/useForm';
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
-import { CURRENT_USER_QUERY } from './User';
+import Form from './styles/Form';
+import useForm from '../lib/useForm';
 import Error from './ErrorMessage';
 
 const REQUEST_RESET_MUTATION = gql`
@@ -22,42 +21,40 @@ export default function RequestReset() {
     REQUEST_RESET_MUTATION,
     {
       variables: inputs,
-      // refetch the currently logged in user
+      // refectch the currently logged in user
       // refetchQueries: [{ query: CURRENT_USER_QUERY }],
     },
   );
-
   async function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault(); // stop the form from submitting
     console.log(inputs);
-    const res = await signup();
+    const res = await signup().catch(console.error);
     console.log(res);
     console.log({ data, loading, error });
     resetForm();
     // Send the email and password to the graphqlAPI
   }
-
   return (
     <Form method='POST' onSubmit={handleSubmit}>
       <h2>Request a Password Reset</h2>
       <Error error={error} />
-      {data?.sendUserPasswordResetLink === null && (
-        <p>Success! Check your email for a link!</p>
-      )}
-      <fieldset disabled={data?.sendUserPasswordResetLink === null}>
+      <fieldset>
+        {data?.sendUserPasswordResetLink === null && (
+          <p>Success! Check your email for a link!</p>
+        )}
+
         <label htmlFor='email'>
           Email
           <input
             type='email'
             name='email'
-            id='email'
             placeholder='Your Email Address'
             autoComplete='email'
             value={inputs.email}
             onChange={handleChange}
           />
         </label>
-        <button type='submit'>Request Reset</button>
+        <button type='submit'>Request Reset!</button>
       </fieldset>
     </Form>
   );
