7ec9d4f751f3262b903075198a3b034b89ca760a
ClaytonDewey <clay@dryadmedia.com>
2026-04-07 12:24:20 -0500

    Add user roles/permissions

--- sick-fits/backend/schemas/ProductImage.ts
+++ sick-fits/backend/schemas/ProductImage.ts
@@ -2,15 +2,22 @@ import 'dotenv/config';
 import { relationship, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 import { cloudinaryImage } from '@keystone-next/cloudinary';
+import { isSignedIn, permissions } from '../access';
 
 export const cloudinary = {
-  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-  apiKey: process.env.CLOUDINARY_KEY,
-  apiSecret: process.env.CLOUDINARY_SECRET,
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME as string,
+  apiKey: process.env.CLOUDINARY_KEY as string,
+  apiSecret: process.env.CLOUDINARY_SECRET as string,
   folder: 'sickfits',
 };
 
 export const ProductImage = list({
+  access: {
+    create: isSignedIn,
+    read: () => true,
+    update: permissions.canManageProducts,
+    delete: permissions.canManageProducts,
+  },
   fields: {
     image: cloudinaryImage({
       cloudinary,
