eeb7a42d50fb05498674cb06317570c688de9796
ClaytonDewey <clay@dryadmedia.com>
2026-04-08 12:29:39 -0500

    Run nav tests

--- sick-fits/frontend/__tests__/Nav.test.js
+++ sick-fits/frontend/__tests__/Nav.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { MockedProvider } from '@apollo/react-testing';
-import { Nav } from '../components/Nav';
+import Nav from '../components/Nav';
 import { CURRENT_USER_QUERY } from '../components/User';
 import { fakeUser, fakeCartItem } from '../lib/testUtils';
 import { CartStateProvider } from '../lib/cartState';
@@ -33,8 +33,6 @@ const signedInMocksWithCartItems = [
   },
 ];
 
-console.log(signedInMocksWithCartItems);
-
 describe('<Nav/>', () => {
   it('Renders and minimal nav when signed out', () => {
     const { container, debug } = render(
@@ -44,6 +42,38 @@ describe('<Nav/>', () => {
         </MockedProvider>
       </CartStateProvider>,
     );
-    debug();
+    expect(container).toHaveTextContent('Sign In');
+    expect(container).toMatchSnapshot();
+    const link = screen.getByText('Sign In');
+    expect(link).toHaveAttribute('href', '/signin');
+    const productsLink = screen.getByText('Products');
+    expect(productsLink).toBeInTheDocument();
+    expect(productsLink).toHaveAttribute('href', '/products');
+  });
+
+  it('renders a full nav when signed in', async () => {
+    const { container, debug } = render(
+      <CartStateProvider>
+        <MockedProvider mocks={signedInMocks}>
+          <Nav />
+        </MockedProvider>
+      </CartStateProvider>,
+    );
+    await screen.findByText('Account');
+    expect(container).toMatchSnapshot();
+    expect(container).toHaveTextContent('Sign Out');
+    expect(container).toHaveTextContent('My Cart');
+  });
+
+  it('renders the amount of items in the cart', async () => {
+    const { container, debug } = render(
+      <CartStateProvider>
+        <MockedProvider mocks={signedInMocksWithCartItems}>
+          <Nav />
+        </MockedProvider>
+      </CartStateProvider>,
+    );
+    await screen.findByText('Account');
+    expect(screen.getByText('3')).toBeInTheDocument();
   });
 });
